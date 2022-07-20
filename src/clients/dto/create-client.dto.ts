f2a1b0b31f615cd1722fbac95ba3b335775658a5
vitor-pereira-gaivota <vitor.pereira@gaivota.ai>
2022-07-20 18:18:37 -0300

    feat: update dto clients

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,33 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
-import {
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-  ValidateNested,
-} from 'class-validator';
-
-class PeopleData {
-  @ApiProperty({ required: false })
-  @IsNumber()
-  @IsOptional()
-  id: number;
-
-  @ApiProperty({ required: true })
-  @IsString()
-  name: string;
-
-  @ApiProperty({ required: true })
-  @IsString()
-  documentNumber: string;
-}
-
-class localData {
-  @ApiProperty({ required: true })
-  @IsNumber()
-  id: number;
-}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateClientDto {
   @ApiProperty({ required: true })
@@ -39,14 +11,4 @@ export class CreateClientDto {
   @IsNumber()
   @IsNotEmpty()
   clientTypeId: number;
-
-  @ApiProperty({ type: () => PeopleData, isArray: true })
-  @Type(() => PeopleData)
-  @ValidateNested({ each: true })
-  peoples: PeopleData[];
-
-  @ApiProperty({ type: () => localData, isArray: true })
-  @Type(() => localData)
-  @ValidateNested({ each: true })
-  locals: localData[];
 }
--- src/clients/dto/update-client.dto.ts
+++ src/clients/dto/update-client.dto.ts
@@ -1,4 +1,28 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNumber, ValidateNested } from 'class-validator';
 import { CreateClientDto } from './create-client.dto';
 
-export class UpdateClientDto extends PartialType(CreateClientDto) {}
+class PeopleData {
+  @ApiProperty({ required: false })
+  @IsNumber()
+  id: number;
+}
+
+class localData {
+  @ApiProperty({ required: true })
+  @IsNumber()
+  id: number;
+}
+
+export class UpdateClientDto extends PartialType(CreateClientDto) {
+  @ApiProperty({ type: () => PeopleData, isArray: true })
+  @Type(() => PeopleData)
+  @ValidateNested({ each: true })
+  peoples: PeopleData[];
+
+  @ApiProperty({ type: () => localData, isArray: true })
+  @Type(() => localData)
+  @ValidateNested({ each: true })
+  locals: localData[];
+}
